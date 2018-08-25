fbcf4ebfaac9def6e8a138a546f8325becd59ff4
AgentGen <42672476+AgentGen@users.noreply.github.com>
2018-08-26 00:38:22 +0300

    1

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
-echo "#!/bin/bash
+
+sudo echo "#!/bin/bash
 sudo apt-get install sshpass -y
 ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N '' -q
 ssh-keyscan -H server2 >> ~/.ssh/known_hosts
 sshpass -p 'vagrant' ssh-copy-id vagrant@server2
-" > ./myscript.sh
-sudo export VISUAL=vim
-sudo export EDITOR=vim
-sudo chmod +x /home/vagrant/myscript.sh
-echo '@reboot sleep 120 && /home/vagrant/myscript.sh' | crontab -
+sudo apt-get install sshpass -y" > /etc/profile.d/myscript.sh
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
-echo "#!/bin/bash
+sudo echo "#!/bin/bash
 sudo apt-get install sshpass -y
 ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N '' -q
 ssh-keyscan -H server1 >> ~/.ssh/known_hosts
 sshpass -p 'vagrant' ssh-copy-id vagrant@server1
-" > ./myscript.sh
-sudo export VISUAL=vim
-sudo export EDITOR=vim
-sudo chmod +x /home/vagrant/myscript.sh
-echo '@reboot sleep 120 && /home/vagrant/myscript.sh' | crontab -
+sudo apt-get install sshpass -y " > //myscript.sh
 
