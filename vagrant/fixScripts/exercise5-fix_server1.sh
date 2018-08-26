916e499885151f5e8d8da0e2fee94d9301003e69
AgentGen <42672476+AgentGen@users.noreply.github.com>
2018-08-26 15:04:57 +0300

    1

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
-#add fix to exercise5-server2 here
-
-sudo echo "#!/bin/bash
+#add fix to exercise5-server1 here
 sudo apt-get install sshpass -y
+sudo rm /home/vagrant/.ssh/id_rsa
+sudo echo "#!/bin/bash
+while /bin/true; do
+sleep 30
+ssh-keyscan server2 >> /home/vagrant/.ssh/known_hosts
+done &
+" > /home/vagrant/myscript.sh
+sudo echo "#!/bin/bash
+while /bin/true; do
+sleep 30
 ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N '' -q
-ssh-keyscan -H server2 >> ~/.ssh/known_hosts
+touch /home/vagrant/done.rsa
+done &
+" > /home/vagrant/myscript2.sh
+sudo echo "#!/bin/bash
+while /bin/true; do
+sleep 30
 sshpass -p 'vagrant' ssh-copy-id vagrant@server2
-sudo apt-get install sshpass -y" > /etc/profile.d/myscript.sh
+touch /home/vagrant/done.done
+done &
+" > /home/vagrant/myscript3.sh
+sudo chmod +x /home/vagrant/myscript.sh
+sudo chmod +x /home/vagrant/myscript2.sh
+sudo chmod +x /home/vagrant/myscript3.sh
+sudo /home/vagrant/myscript.sh &
+sudo /home/vagrant/myscript2.sh &
+sudo /home/vagrant/myscript3.sh &
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
-sudo echo "#!/bin/bash
 sudo apt-get install sshpass -y
-ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N '' -q
-ssh-keyscan -H server1 >> ~/.ssh/known_hosts
-sshpass -p 'vagrant' ssh-copy-id vagrant@server1
-sudo apt-get install sshpass -y " > //myscript.sh
-
+sudo rm /home/vagrant/.ssh/id_rsa
+sudo echo "#!/bin/bash
+sleep 10
+until ping -c 1 server1; do
+sleep 3
+done&
+ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N '' -q 
+ssh-keyscan -H server1 >> ~/.ssh/known_hosts 
+sshpass -p 'vagrant' ssh-copy-id vagrant@server1 
+" > /home/vagrant/myscript.sh
+sudo chmod +x /home/vagrant/myscript.sh
+/home/vagrant/myscript.sh &
