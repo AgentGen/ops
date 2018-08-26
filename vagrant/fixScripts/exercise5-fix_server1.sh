a6a22cde8f1b166aa0abd48c6ed49964d990dcbd
AgentGen <42672476+AgentGen@users.noreply.github.com>
2018-08-26 16:18:34 +0300

    1

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -3,28 +3,17 @@
 sudo apt-get install sshpass -y
 sudo rm /home/vagrant/.ssh/id_rsa
 sudo echo "#!/bin/bash
-while /bin/true; do
-sleep 30
-ssh-keyscan server2 >> /home/vagrant/.ssh/known_hosts
-done &
-" > /home/vagrant/myscript.sh
-sudo echo "#!/bin/bash
-while /bin/true; do
-sleep 30
+sleep 60
+touch /home/vagrant/.ssh/known_hosts
+chmod 777 /home/vagrant/.ssh/known_hosts
+echo "StrictHostKeyChecking=no" > /home/vagrant/.ssh/config
+sudo service ssh restart
+#touch /home/vagrant/done.host
 ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N '' -q
-touch /home/vagrant/done.rsa
-done &
-" > /home/vagrant/myscript2.sh
-sudo echo "#!/bin/bash
-while /bin/true; do
-sleep 30
-sshpass -p 'vagrant' ssh-copy-id vagrant@server2
-touch /home/vagrant/done.done
-done &
-" > /home/vagrant/myscript3.sh
+#touch /home/vagrant/done.rsa
+sshpass -p 'vagrant' ssh-copy-id vagrant@server2 
+#touch /home/vagrant/done.done
+echo $0
+" > /home/vagrant/myscript.sh
 sudo chmod +x /home/vagrant/myscript.sh
-sudo chmod +x /home/vagrant/myscript2.sh
-sudo chmod +x /home/vagrant/myscript3.sh
-sudo /home/vagrant/myscript.sh &
-sudo /home/vagrant/myscript2.sh &
-sudo /home/vagrant/myscript3.sh &
+sudo su -c /home/vagrant/myscript.sh -s /bin/sh vagrant &
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -3,13 +3,17 @@
 sudo apt-get install sshpass -y
 sudo rm /home/vagrant/.ssh/id_rsa
 sudo echo "#!/bin/bash
-sleep 10
-until ping -c 1 server1; do
-sleep 3
-done&
-ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N '' -q 
-ssh-keyscan -H server1 >> ~/.ssh/known_hosts 
-sshpass -p 'vagrant' ssh-copy-id vagrant@server1 
+sleep 20
+touch /home/vagrant/.ssh/known_hosts
+chmod 777 /home/vagrant/.ssh/known_hosts
+echo "StrictHostKeyChecking=no" > /home/vagrant/.ssh/config
+sudo service ssh restart
+#touch /home/vagrant/done.host
+ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N '' -q
+#touch /home/vagrant/done.rsa
+sshpass -p 'vagrant' ssh-copy-id vagrant@server1
+#touch /home/vagrant/done.done
+echo $0
 " > /home/vagrant/myscript.sh
 sudo chmod +x /home/vagrant/myscript.sh
-/home/vagrant/myscript.sh &
+sudo su -c /home/vagrant/myscript.sh -s /bin/sh vagrant &
